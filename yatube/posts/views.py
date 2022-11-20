0191822b97b5db07a6f571532b2041f0fb99d043
Maksim Sablin <maximsablin.spb@gmail.com>
2022-11-20 15:30:47 +0300

    added templates dir; created base, header, footer, index, group_list html files

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Index page first Django project. Yahooo!')
+    template = 'posts/index.html'
+    return render(request, template)
 
 def group_posts_list(request):
-    return HttpResponse('Group list is here')
+    template = 'posts/group_list.html'
+    return render(request, template)
 def group_post_detail(request, slug):
     return HttpResponse(f'Post number {slug}')
 
