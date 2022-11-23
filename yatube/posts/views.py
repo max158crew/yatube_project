142104ee6f0d08588dfb29a7b469bed85ec9eb5b
Maksim Sablin <maximsablin.spb@gmail.com>
2022-11-23 14:10:30 +0300

    'index' view function connected with db in index.html added 'for' cycle for posts publishing from database

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Post
 
 def index(request):
-    template = 'posts/index.html'
-    title = 'Это главная страница проекта Yatube'
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
     context = {
-        'title': title,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
+
+# def index(request):
+#     template = 'posts/index.html'
+#     title = 'Это главная страница проекта Yatube'
+#     context = {
+#         'title': title,
+#     }
+#     return render(request, template, context)
 
 
 def group_posts(request):
